81332b5da9f80aa8ace85b86fe159f6fbb98ec5e
Lea Linin <lea.linin@gmail.com>
2020-01-09 03:39:29 +0000

    Add query that selects details about a single user

---
+++ 1_queries/1_user_login.sql
@@ -0,0 +1,18 @@
+-- Get details about a single user.
+-- Select their id, name, email, and password.
+-- Select a single user using their email address. Use tristanjacobs@gmail.com for now.
+
+-- SELECT count(assistance_requests.*) as total_assistances, teachers.name
+-- FROM assistance_requests
+-- JOIN teachers ON teachers.id = teacher_id
+-- WHERE name = 'Waylon Boehm'
+-- GROUP BY teachers.name;
+
+-- id |     name      |          email          | password
+-- ----+---------------+-------------------------+---------
+--   1 | Devin Sanders | tristanjacobs@gmail.com | $2a...
+-- (1 row)
+
+SELECT id, name, email, password
+FROM users
+WHERE email = 'tristanjacobs@gmail.com';
