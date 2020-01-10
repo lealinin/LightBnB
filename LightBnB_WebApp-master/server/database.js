3dd23191ce8827cd5506647124c79ba8c62dacc7
Lea Linin <lea.linin@gmail.com>
2020-01-10 03:09:57 +0000

    Add get reservations query inside function

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -117,10 +117,47 @@ exports.addUser = addUser;
  * @param {string} guest_id The id of the user.
  * @return {Promise<[{}]>} A promise to the reservations.
  */
+
+// This function accepts a guest_id, limits the properties to 10 and returns a promise. 
+// The promise should resolve reservations for that user. 
+// Use the All My Reservations query that you made in a previous assignments.
+
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool.query(`
+  SELECT properties.*, reservations.*, avg(rating) as average_rating
+  FROM reservations
+  JOIN properties ON reservations.property_id = properties.id
+  JOIN property_reviews ON properties.id = property_reviews.property_id 
+  WHERE reservations.guest_id = $1
+  GROUP BY properties.id, reservations.id
+  ORDER BY reservations.start_date
+  LIMIT $2`, [guest_id, limit])
+  .then (res => {
+    if (res.rows.length) {
+      return res.rows;
+    }
+    return null;
+  });
 }
+
+// const getUserWithId = function(id) {
+//   return pool.query(`
+//   SELECT * FROM users
+//   WHERE id = $1
+//   `, [id])
+//   .then(res => {
+//     if (res.rows.length) {
+//       return res.rows[0];
+//     } 
+//     return null;
+//   });
+// }
+
 exports.getAllReservations = getAllReservations;
+
+// const getAllReservations = function(guest_id, limit = 10) {
+//   return getAllProperties(null, 2);
+// }
 
 /// Properties
 
