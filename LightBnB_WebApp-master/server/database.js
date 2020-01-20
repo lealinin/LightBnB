b00fde5709c03a57fee874c81238820e2d3aeccb
Lea Linin <lea.linin@gmail.com>
2020-01-20 03:28:18 +0000

    Replace pool with db

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -1,13 +1,4 @@
-const properties = require('./json/properties.json');
-const users = require('./json/users.json');
-const { Pool } = require('pg');
-
-const pool = new Pool({
-  user: 'vagrant',
-  password: '123',
-  host: 'localhost',
-  database: 'lightbnb'
-});
+const db = require('../db');
 
 /// Users
 
@@ -18,7 +9,7 @@ const pool = new Pool({
  */
 
 const getUserWithEmail = function (email) {
-  return pool.query(`
+  return db.query(`
   SELECT * FROM users
   WHERE email = $1
   `, [email])
@@ -39,7 +30,7 @@ exports.getUserWithEmail = getUserWithEmail;
  */
 
 const getUserWithId = function (id) {
-  return pool.query(`
+  return db.query(`
   SELECT * FROM users
   WHERE id = $1
   `, [id])
@@ -60,7 +51,7 @@ exports.getUserWithId = getUserWithId;
  */
 
 const addUser = function (user) {
-  return pool.query(`
+  return db.query(`
   INSERT INTO users (name, email, password)
   VALUES ($1, $2, $3)
   RETURNING*;
@@ -84,7 +75,7 @@ exports.addUser = addUser;
  */
 
 const getAllReservations = function (guest_id, limit = 10) {
-  return pool.query(`
+  return db.query(`
   SELECT properties.*, reservations.*, avg(rating) as average_rating
   FROM reservations
   JOIN properties ON reservations.property_id = properties.id
@@ -162,7 +153,7 @@ const getAllProperties = function (options, limit = 10) {
 
   console.log(queryString, queryParams);
 
-  return pool.query(queryString, queryParams)
+  return db.query(queryString, queryParams)
     .then(res => res.rows);
 }
 
@@ -174,7 +165,7 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function (property) {
-  return pool.query(`
+  return db.query(`
   INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street, city, province, post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms)
   VALUES
   ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
